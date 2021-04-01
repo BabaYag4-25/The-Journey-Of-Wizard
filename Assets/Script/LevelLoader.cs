6a5942096a98786ffa28e76abea46c27b87e42dc
BabaYag4-25 <ahmadghif25@gmail.com>
2021-04-01 15:14:30 +0700

    add transisi beta

---
+++ Assets/Script/LevelLoader.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+
+    public Animator transisi;
+
+    public float waktuTransisi = 1f;
+    
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            LoadSelectLevel();
+        }
+    }
+
+    public void LoadSelectLevel()
+    {
+        StartCoroutine(LoadScene(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadScene(int sceneIndex)
+    {
+        //play animasi
+        transisi.SetTrigger("Start");
+
+        //wait
+        yield return new WaitForSeconds(waktuTransisi);
+
+        //load scene
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
